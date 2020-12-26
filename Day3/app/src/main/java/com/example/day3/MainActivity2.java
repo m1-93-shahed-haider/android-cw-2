d290a31562a9a3403192f6d7d5220b142cd02816
m1-93-shahed-haider <shahedhaitham@gmail.com>
2020-12-26 16:05:06 +0300

    send email added

--- Day3/app/src/main/java/com/example/day3/MainActivity2.java
+++ Day3/app/src/main/java/com/example/day3/MainActivity2.java
@@ -29,7 +29,11 @@ public class MainActivity2 extends AppCompatActivity {
         TextView www= findViewById(R.id.w2);
         www.setText(pp);
 
-        
+        TextView wwww= findViewById(R.id.w3);
+        wwww.setText(ee);
+
+
+
 
         www.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +44,23 @@ public class MainActivity2 extends AppCompatActivity {
             }
         });
 
+
+        String subject = "Hello Message";
+        String message = "Hello";
+
+        wwww.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_EMAIL, ee);
+                email.putExtra(Intent.EXTRA_SUBJECT, subject);
+                email.putExtra(Intent.EXTRA_TEXT, message);
+                email.setType("text/plain");
+                startActivity(Intent.createChooser(email,"Send email:"));
+
+
+            }
+        });
 
     }
 }
